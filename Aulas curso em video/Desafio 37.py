f4aeb167aceeaed0b64ba36ccf2c4337a9365b38
NataliaBento <nataliabento97@gmail.com>
2024-04-03 16:00:12 -0300

    Desafio 37

--- Aulas curso em video/Desafio 37.py
+++ Aulas curso em video/Desafio 37.py
@@ -1,14 +1,14 @@
-num = int(input('Digite um número inteiro:'))
+num = int(input('Digite um número inteiro: '))
 print('''Escolha uma das bases para conversão:
       [1] converter para BINÁRIO
       [2] converter para OCTAL
       [3] converter para HEXADECIMAL''')
 opcao = int(input('Sua opção: '))
 if opcao == 1:
-    print(f'{num} convertido para BINÁRIO é igual a {bin(num)}')
+   print(f'{num} convertido para BINÁRIO é igual a: {bin(num)}')
 elif opcao == 2:
-    print(f'{num} convertido para OCTAL é igual a {oct(num)}')
+   print(f'{num} convertido para OCTAL é igual a: {oct(num)} ')
 elif opcao == 3:
-    print(f'{num} convertido para HEXADECIMAL ')
-    print()
-    
+   print(f'{num} convertido para HEXADECIMAL é igual á: {hex(num)}')
+else:
+   print('Não exste opção valida')
