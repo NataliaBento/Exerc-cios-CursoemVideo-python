f3d2889c490907e1a308b437e67c19ffce64e725
NataliaBento <nataliabento97@gmail.com>
2024-04-04 10:34:32 -0300

    desafio comparando numeros

---
+++ Aulas curso em video/Desafio 38.py
@@ -0,0 +1,8 @@
+primeiro = int(input('Primeiro número: '))
+segundo = int(input('Segundo número: '))
+if primeiro > segundo:
+    print('O primeiro valor é maior!')
+elif primeiro < segundo:
+    print('O segundo valor é maior!')
+else:
+    print('Os valores são iguais!')
