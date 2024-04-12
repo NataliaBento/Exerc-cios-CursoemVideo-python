f34f9f5fa4f5caae915453feb71d29f7ac70d37e
NataliaBento <nataliabento97@gmail.com>
2024-04-12 14:20:49 -0300

    aula 13

---
+++ Aulas curso em video/aula 13.py
@@ -0,0 +1,12 @@
+#i = int(input('Digite o início: '))
+#f = int(input('Digite o fim: '))
+#p = int(input('Digite o passo: '))
+#for c in range (i, f+1, p):
+    #print(c)
+#print('FIM')
+
+s = 0
+for c in range (0,4):
+    n = int(input('Digite um valor: '))
+    s = s + n
+print(f'o somatório dos numeros é {s}')
