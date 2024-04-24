eb8e8d51457142d9c1e2a144c29bf627c96384fa
NataliaBento <nataliabento97@gmail.com>
2024-04-24 14:48:51 -0300

    Desafio 50

--- Aulas curso em video/Desafio 50.py
+++ Aulas curso em video/Desafio 50.py
@@ -1,1 +1,11 @@
-   
+soma = 0
+cont = 0
+contpar = 0
+for c in range (1, 7):
+    num = int(input("Digite um número: "))
+    cont = cont + 1
+    if num % 2 == 0:
+        soma = soma + num
+        contpar = contpar + 1
+print(f"Você digitou {cont} números, sendo {contpar} PARES e a soma entre eles é: {soma}")
+
