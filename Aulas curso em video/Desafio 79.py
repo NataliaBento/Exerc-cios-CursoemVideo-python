c694864cf8dc33089bac2132d17fce454a32a279
NataliaBento <nataliabento97@gmail.com>
2024-07-09 09:07:02 -0300

    Desafio 79

---
+++ Aulas curso em video/Desafio 79.py
@@ -0,0 +1,16 @@
+numero = []
+
+while True:
+    n = int(input('Digite um valor: '))
+    if n not in numero:
+        numero.append(n)
+        print('Valor adicionado com sucesso!')
+    else:
+        print('Valor duplicado! Não vou adicionar!')
+    continua = str(input('Quer continuar? [S/N]')).upper()[0]
+    if continua in 'Nn':
+        break
+numero.sort()
+print('='*30)
+print(f'Você digitou os valoes: {numero}')
+
