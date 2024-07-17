46fe8985846f21d115bebe910f539fa712734120
Natalia Bento <nataliabento97@gmail.com>
2024-07-17 15:41:24 -0300

    Desafio 84

--- Aulas curso em video/Desafio 84.py
+++ Aulas curso em video/Desafio 84.py
@@ -1,14 +1,32 @@
 temp = []
 princ = []
-totmaior = totmenor = 0
-while True:
+maior = menor = 0
+while True: 
     temp.append(str(input('Nome: ')))
-    temp.append(int(input('Peso: ')))
-    continuar = (str(input('Quer continuar? [S/N]')))
-    temp.append(temp[:])
+    temp.append(float(input('Peso: ')))
+    if len(princ) == 0:
+        maior = temp [1]
+        menor = temp [1]
+    else:
+        if temp[1] > maior:
+            maior = temp[1]
+        if temp[1] < menor:
+            menor = temp[1]
+    princ.append(temp[:])
     temp.clear()
-    print(princ) #evita que dentro do looping os numeros de repitam a cada iteração
-    if continuar in 'Nn':
-        print('Programa finalizado')
-        break
 
+    resp = str(input('Quer continuar? [S/N]'))
+    if resp in 'Nn':
+        break
+print('='*30)
+print(princ)
+print(f'Ao todo você cadastrou: {len(princ)} pessoas')
+print(f'O maior peso foi de {maior} kgs. Peso de ', end='')
+for p in princ:
+    if p[1] == maior:
+        print(f'{p[0]} ', end='')
+print(f'O menor peso foi de {menor} kgs. Peso de ', end='')
+print()
+for p in princ:
+    if p[1] == menor:
+        print(f'{p[0]} ', end='')
