cb37c4a1466546d9288594dd3478d132e00e8803
NataliaBento <nataliabento97@gmail.com>
2024-07-29 14:03:06 -0300

    Desafio 86 em construção

--- Aulas curso em video/Desafio 86.py
+++ Aulas curso em video/Desafio 86.py
@@ -1,4 +1,5 @@
 matriz = [ [0, 0, 0], [0, 0 , 0], [0, 0, 0]]
+spar = mai = scol = 0
 for l in range (0, 3):
     for c in range (0,3):
         matriz [l] [c] = int(input(f'Digite um valor para [{l}, {c}]:'))
@@ -6,6 +7,12 @@ print('='* 40)
 for l in range (0, 3):
     for c in range (0, 3):
         print(f'[{matriz[l] [c]:^5}]', end='')
+        if matriz [l] [c] % 2 ==0:
+            spar += matriz [l][c]
+        if len(matriz[c]) == 2:
+            scol += matriz[c]
     print()
-
-    
+print('=' * 40)
+print(f'A soma dos numeros pares é: {spar}')
+print(f'O maior número digitado: {mai}')
+print(f'A soma da coluna é: {scol}')
